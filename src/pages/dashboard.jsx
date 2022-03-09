cf5cbfc144c9a9b6c11f1b4fb8f0b7040b77fca2
wurdig-mich <harshay.khetan@gmail.com>
2022-03-09 17:03:52 +0530

    dashboard

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import NavBar from '../components/dNavBar'
+const Index = () => {
+    return (
+        <div className="">
+            <NavBar />
+        </div>
+    );
+};
+
+export default Index;
